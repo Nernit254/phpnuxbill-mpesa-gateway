bdc6f1736e0e2540f274e5fb4683b83f16c7f2d1
Nernit254 <nernitenterprises@gmail.com>
2025-05-09 19:14:16 +0300

    Create en.php

---
+++ mpesa/lang/en.php
@@ -0,0 +1,16 @@
+<?php
+$_L['mpesa_push_payment'] = "Pay via MPESA STK Push";
+$_L['mpesa_initiate_payment'] = "Initiate MPESA Payment";
+$_L['mpesa_processing'] = "Processing your request...";
+$_L['mpesa_push_success'] = "Payment request sent to your phone. Please check your MPESA menu to complete payment.";
+$_L['mpesa_push_failed'] = "Failed to initiate payment. Please try again or use manual method below.";
+$_L['mpesa_network_error'] = "Network error. Please try again.";
+$_L['mpesa_manual_payment'] = "Manual MPESA Payment";
+$_L['mpesa_step1'] = "Go to your MPESA menu";
+$_L['mpesa_step2'] = "Select \"Lipa na MPESA\"";
+$_L['mpesa_step3'] = "Select \"Pay Bill\"";
+$_L['mpesa_step4'] = "Enter Business Number: %s";
+$_L['mpesa_step5'] = "Enter Account Number: %s";
+$_L['mpesa_step6'] = "Enter Amount: %s %s";
+$_L['mpesa_step7'] = "Enter your MPESA PIN and confirm";
+$_L['mpesa_processing_time'] = "Your payment will be processed automatically within a few minutes.";
